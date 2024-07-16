a5f8c0ccfff26e8b79647ca8f1068c5e9c655d18
CyberFrontEnd <avoelike@gmail.com>
2024-07-16 23:19:33 +0400

    End day 16,07,2024

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,4 +17,26 @@ $(document).ready(function () {
   }
 
   phoneMask();
+
+  // Start mySwiper
+  const progressCircle = document.querySelector(".autoplay-progress svg");
+  const progressContent = document.querySelector(".autoplay-progress span");
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    // autoplay: {
+    //   delay: 5500,
+    //   disableOnInteraction: false
+    // },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    on: {
+      autoplayTimeLeft(s, time, progress) {
+        progressCircle.style.setProperty("--progress", 1 - progress);
+        progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+      }
+    }
+  });
 });
